f75207a41c25f7b9c9e575a317bc7aa6487daf27
C. Bess <cbess@quantumquinn.com>
2014-02-23 01:01:19 -0600

    Initial Commit

---
+++ CBSearchKitTests/CBSearchKitTests.m
@@ -0,0 +1,34 @@
+//
+//  CBSearchKitTests.m
+//  CBSearchKitTests
+//
+//  Created by C. Bess on 2/23/14.
+//  Copyright (c) 2014 C. Bess. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CBSearchKitTests : XCTestCase
+
+@end
+
+@implementation CBSearchKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
