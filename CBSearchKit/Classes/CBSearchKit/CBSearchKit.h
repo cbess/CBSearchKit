57c937db8eaabc6e65a4f72a3682f2e0ae51c557
C. Bess <cbess@quantumquinn.com>
2014-07-21 23:50:11 -0500

    - refactor search kit header

--- CBSearchKit/Classes/CBSearchKit/CBSearchKit.h
+++ CBSearchKit/Classes/CBSearchKit/CBSearchKit.h
@@ -1,15 +1,14 @@
 //
-//  CBSSearchKit.h
+//  CBSearchKit.h
 //  CBSearchKit
 //
 //  Created by C. Bess on 2/24/14.
 //  Copyright (c) 2014 C. Bess. All rights reserved.
 //
 
-#ifndef CBSearchKit_CBSSearchKit_h
-#define CBSearchKit_CBSSearchKit_h
+#ifndef CBSearchKit_CBSearchKit_h
+#define CBSearchKit_CBSearchKit_h
 
-#import "CBSMacros.h"
 #import "CBSIndexer.h"
 #import "CBSSearcher.h"
 #import "CBSSearchManager.h"
